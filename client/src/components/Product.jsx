b2a54bfcebf054973409fc2afca37adda5cbdc67
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-06-05 21:51:10 -0400

    set up Newsletter and Footer components

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import styled from 'styled-components';
 import { SearchOutlined, ShoppingCartOutlined, FavoriteBorderOutlined } from '@material-ui/icons';
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,6 +4,8 @@ import Categories from '../components/Categories';
 import Navbar from '../components/Navbar';
 import Slider from '../components/Slider';
 import Products from '../components/Products';
+import Newsletter from '../components/Newsletter';
+import Footer from '../components/Footer';
 
 const Home = () => {
 	return (
@@ -13,6 +15,8 @@ const Home = () => {
 			<Slider />
 			<Categories />
 			<Products />
+			<Newsletter />
+			<Footer />
 		</div>
 	);
 };

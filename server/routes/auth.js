012f38399dff437d29333f974514ac0fa1e97d39
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-06-13 01:44:30 -0400

    setup jwt auth and update user

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const CryptoJS = require('crypto-js');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/User');
 
@@ -32,8 +33,19 @@ router.post('/login', async (req, res) => {
 
 		originalPassword !== req.body.password && res.status(401).json('Wrong credentials!');
 
+		const accessToken = jwt.sign(
+			{
+				id: user._id,
+				isAdmin: user.isAdmin
+			},
+			process.env.JWT_SECRET,
+			{
+				expiresIn: '3d'
+			}
+		);
+
 		const { password, ...others } = user._doc;
-		res.status(200).json(others);
+		res.status(200).json({ ...others, accessToken });
 	} catch (err) {
 		res.status(500).json(err);
 	}

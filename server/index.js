42408d8d8e777903da16690ad56b7525468cd683
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-06-15 02:06:29 -0400

    set up products api routes

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/user');
+const productRoute = require('./routes/product');
 
 mongoose
 	.connect(process.env.MONGO_URL)
@@ -21,6 +22,7 @@ app.get('/api/healthcheck', () => {
 
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
+app.use('/api/products', productRoute);
 
 app.listen(process.env.PORT || 5000, () => {
 	console.log('Backend server is running');

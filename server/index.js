fa2cfed6ebc040d35936aa6e6a94a9eeeb3fe15f
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-06-17 01:54:09 -0400

    Set up the rest of the API endpoints:

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ const mongoose = require('mongoose');
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/user');
 const productRoute = require('./routes/product');
+const cartRoute = require('./routes/cart');
+const orderRoute = require('./routes/order');
 
 mongoose
 	.connect(process.env.MONGO_URL)
@@ -23,6 +25,8 @@ app.get('/api/healthcheck', () => {
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/products', productRoute);
+app.use('/api/carts', cartRoute);
+app.use('/api/orders', orderRoute);
 
 app.listen(process.env.PORT || 5000, () => {
 	console.log('Backend server is running');

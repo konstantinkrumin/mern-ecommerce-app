d93bf8e84583d006ff3a1811dd0583ac22cc0c37
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-06-17 02:27:34 -0400

    added stripe route to index

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ const userRoute = require('./routes/user');
 const productRoute = require('./routes/product');
 const cartRoute = require('./routes/cart');
 const orderRoute = require('./routes/order');
+const stripeRoute = require('./routes/stripe');
 
 mongoose
 	.connect(process.env.MONGO_URL)
@@ -29,6 +30,7 @@ app.use('/api/users', userRoute);
 app.use('/api/products', productRoute);
 app.use('/api/carts', cartRoute);
 app.use('/api/orders', orderRoute);
+app.use('/api/checkout', stripeRoute);
 
 app.listen(process.env.PORT || 5000, () => {
 	console.log('Backend server is running');

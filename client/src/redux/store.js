96ddc6fd5b6d3f87881ac03dc683081b26c92ed9
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-06-22 22:58:24 -0400

    added admin UI

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,4 +1,4 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import {
 	persistStore,
 	persistReducer,
@@ -20,13 +20,12 @@ const persistConfig = {
 	storage
 };
 
-const persistedReducer = persistReducer(persistConfig, userReducer);
+const rootReducer = combineReducers({ user: userReducer, cart: cartReducer });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-	reducer: {
-		cart: cartReducer,
-		user: persistedReducer
-	},
+	reducer: persistedReducer,
 	middleware: getDefaultMiddleware =>
 		getDefaultMiddleware({
 			serializableCheck: {
